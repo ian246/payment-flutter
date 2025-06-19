0a6a6ed52b7937d06c38b4e452b42755491633db
Ian Karel Ctvrtnik <ianctvrtnik@gmail.com>
2025-06-19 16:09:47 -0300

    fist commit

--- lib/constantss.dart
+++
@@ -1,5 +0,0 @@
-// const String stripePublishableKey =
-//     'pk_test_51RblQX07PwlPqKG7k9KN4nyA15uOebkV2Qp0C7x2Q9OpzkqpiEBvgKOcpB9GY7yRvSnPoSrzDbs1Ls5HQL5g2gd3001Cx0mXxL';
-// const String stripeSecretKey =
-//     'sk_test_51RblQX07PwlPqKG7UIAkFN5630dyHOTYThc09tsWAucC7NAgHs2wWPAdzAeUoxgbSOdpp81NrxOGaZsN1OIpGxwj00IUBJ8CQZ';
-// const String baseUrl = 'https://api.stripe.com/v1/payment_intents';
